a0bf76b013c0045ab5a08b64a3bdf583b423ddb4
Ned <iamcalledned@gmail.com>
2025-04-16 19:25:34 -0400

    Update rss_config.py

--- rss_config.py
+++ rss_config.py
@@ -4,6 +4,5 @@ RSS_FEEDS = {
     "Reuters": "https://feeds.reuters.com/reuters/businessNews",
     "ZeroHedge": "https://feeds.feedburner.com/zerohedge/feed",
     "FT": "https://www.ft.com/?format=rss",
-    "Investing.com": "https://www.investing.com/rss/news.rss",
     "CNBC": "https://www.cnbc.com/id/100003114/device/rss/rss.html"
 }
