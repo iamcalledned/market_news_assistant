75012c3c2ee68556e994d18ccbddc8cb1d713c61
Ned <iamcalledned@gmail.com>
2025-04-17 09:35:01 -0400

    Update rss_config.py

--- rss_config.py
+++ rss_config.py
@@ -1,13 +1,18 @@
 # rss_config.py
 
 RSS_FEEDS = {
-    "Reuters": "https://feeds.reuters.com/news/wealth",
+    "Reuters": "https://ir.thomsonreuters.com/rss/news-releases.xml?items=15",
     "ZeroHedge": "https://feeds.feedburner.com/zerohedge/feed",
     "FT": "https://www.ft.com/?format=rss",
     "CNBC": "https://www.cnbc.com/id/100003114/device/rss/rss.html",
-    "Bloomberg Markets": "https://www.bloomberg.com/feed/podcast/etf-report.xml",
-    "WSJ - Markets": "https://feeds.a.dj.com/rss/RSSMarketsMain.xml",
-    "MarketWatch": "https://feeds.marketwatch.com/marketwatch/topstories/",
+    "Bloomberg Markets1": "https://www.bloomberg.com/feed/podcast/etf-report.xml",
+    "Bloomberg Politics": "https://feeds.bloomberg.com/politics/news.rss",
+    "Bloomberg Wealth": "https://feeds.bloomberg.com/wealth/news.rss",
+    "Bloomberg Tech": "https://feeds.bloomberg.com/technology/news.rss",
+    "MarketWatch - Main": "https://www.marketwatch.com/rss",
+    "MarketWatch - Realtime": "https://feeds.content.dowjones.io/public/rss/mw_realtimeheadlines",
+    "MarketWatch - Bulletins": "https://feeds.content.dowjones.io/public/rss/mw_bulletins",
+    "MarketWatch - MarketPulse": "https://feeds.content.dowjones.io/public/rss/mw_marketpulse",
     "Seeking Alpha": "https://seekingalpha.com/market_currents.xml",
     "Yahoo Finance": "https://finance.yahoo.com/news/rssindex",
     "The Market Ear": "https://themarketear.com/feed",
